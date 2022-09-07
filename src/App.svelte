8453a7f6954338212e9ddd58ecaa150fde0fcd82
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2022-09-08 00:01:20 +0200

    Initial commit Basic skeleton, but with animations

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { fade } from 'svelte/transition'
+	import Box from './lib/Box.svelte';
+
+	let boxShown = false;
+    // $: console.log(boxShown);
+	function toggleBox() {
+		boxShown = !boxShown;
+	}
+</script>
+
+<main>
+	<div class="card-container">
+		{#if boxShown}
+            <div
+                class="box"
+                on:click={toggleBox}
+                in:fade
+                out:fade
+            >
+            </div>
+            <Box/>
+		{/if}
+        <button on:click={toggleBox}>Show</button>
+	</div>
+</main>
+
+<style>
+    .card-container {
+        height: min-content;
+    }
+
+    main {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .box {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: #00000061;
+        /* transition: 1000ms; */
+    }
+</style>
---
+++ src/lib/Box.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { scale } from 'svelte/transition';
+
+
+</script>
+
+<div class="card" in:scale out:scale>
+    <h2>Example text</h2>
+</div>
+
+<style>
+    .card {
+        background-color: #242424;
+
+        position:  absolute;
+        top:       50vh;
+        left:      50vw;
+        transform: translate(-50%, -50%);
+
+        width:     700px;
+        height:    400px;
+        z-index:   2137;
+        padding: 2em;
+        text-align: center;
+    }
+</style>
